89c534f305df8264a96ed6b849075a06dd085833
Bartosz Gordon <bartosz.gordon@sealcode.org>
2019-11-30 10:56:21 +0100

    Add cypress test for basic flow

--- cypress/integration/add-habit-scoreboard-item.spec.js
+++ cypress/integration/add-habit-scoreboard-item.spec.js
@@ -7,8 +7,31 @@ describe("Add habit scoreboard item", () => {
 
 	it("basic flow", () => {
 		cy.login("dwight");
-
 		cy.visit(DASHBOARD_URL);
-		cy.findByText("xxx");
+
+		cy.findByLabelText("Habit").type("Wake up at 7:30 AM");
+		cy.findByLabelText("Score").select("positive");
+		cy.findByText("Add habit").click();
+
+		cy.findByText("Habit successfully addedd!");
+
+		cy.findByText("x").click();
+		cy.findByText("Habit successfully addedd!").should("not.exist");
+
+		cy.findByLabelText("Habit").should("have.value", "");
+		cy.findByLabelText("Score").should("have.value", "neutral");
+
+		cy.findByLabelText("Habit").type("Wake up at 7:30 AM");
+		cy.findByLabelText("Score").select("positive");
+		cy.findByText("Add habit").click();
+		cy.findByText("Given habit already exists.");
+
+		cy.findByLabelText("Habit")
+			.clear()
+			.type("Go to sleep at 9:30PAM");
+		cy.findByLabelText("Score").select("positive");
+		cy.findByText("Add habit").click();
+		cy.findByText("Habit successfully addedd!");
+		cy.findByText("x").click();
 	});
 });
