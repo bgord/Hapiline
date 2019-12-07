c557221d53357413d144c3e1b998acc17acf28d5
Bartosz Gordon <bartosz.gordon@sealcode.org>
2019-12-07 10:07:12 +0100

    Handle empty and loading states

--- frontend/src/DashboardWindow.tsx
+++ frontend/src/DashboardWindow.tsx
@@ -3,6 +3,7 @@ import React from "react";
 
 import {CloseableSuccessMessage} from "./SuccessMessages";
 import {ErrorMessage} from "./ErrorMessages";
+import {Loader} from "./Loader";
 import {api} from "./services/api";
 import {useRequestErrors} from "./hooks/useRequestErrors";
 import {useUserProfile} from "./contexts/auth-context";
@@ -113,29 +114,39 @@ export const Dashboard = () => {
 					</ErrorMessage>
 				</Async.IfRejected>
 			</div>
-			<div
-				className="mt-12 bg-white p-4"
-				style={{
-					display: "grid",
-					gridTemplateColumns: "100px 440px",
-					gridTemplateRows: "auto",
-					gridRowGap: "30px",
-				}}
-			>
-				{getHabitScoreboardItemsRequestState?.data?.map(item => (
-					<>
-						<span
-							style={{
-								placeSelf: "center",
-							}}
-							className="bg-gray-300 w-full text-center"
-						>
-							{item.score}
-						</span>
-						<span className="pl-4 10px">{item.name}</span>
-					</>
-				))}
-			</div>
+			<Async.IfPending state={getHabitScoreboardItemsRequestState}>
+				<Loader />
+			</Async.IfPending>
+			<Async.IfFulfilled state={getHabitScoreboardItemsRequestState}>
+				<div
+					className="mt-12 bg-white p-4"
+					style={{
+						display: "grid",
+						gridTemplateColumns: "100px 440px",
+						gridTemplateRows: "auto",
+						gridRowGap: "30px",
+					}}
+				>
+					{!getHabitScoreboardItemsRequestState?.data?.length && (
+						<div className="text-center" style={{gridColumn: "span 2"}}>
+							Seems you haven't added any habits yet.
+						</div>
+					)}
+					{getHabitScoreboardItemsRequestState?.data?.map(item => (
+						<>
+							<span
+								style={{
+									placeSelf: "center",
+								}}
+								className="bg-gray-300 w-full text-center"
+							>
+								{item.score}
+							</span>
+							<span className="pl-4 10px">{item.name}</span>
+						</>
+					))}
+				</div>
+			</Async.IfFulfilled>
 		</section>
 	);
 };
