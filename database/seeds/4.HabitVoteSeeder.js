d46ca65fb9b87d62cc722f9935d04baf2cc77660
Bartosz Gordon <bartosz.gordon@sealcode.org>
2019-12-29 21:27:34 +0100

    Add null votes to seeds

--- database/seeds/4.HabitVoteSeeder.js
+++ database/seeds/4.HabitVoteSeeder.js
@@ -9,17 +9,24 @@ class HabitVoteSeeder {
 		const today = new Date();
 		const habits = await Database.table("habits");
 
+		let lastVoteIndex = 0;
+
 		for (const [index, habit] of habits.entries()) {
 			if (index % 2 === 1) {
 				const creationDate = new Date(habit.created_at);
 
 				const days = datefns.eachDayOfInterval({start: creationDate, end: today});
 
-				const payload = days.map((day, i) => ({
-					day,
-					habit_id: habit.id,
-					vote: [...Object.keys(HABIT_VOTE_TYPES)][i % 3],
-				}));
+				const payload = days.map(day => {
+					const voteIndex = lastVoteIndex;
+					lastVoteIndex = lastVoteIndex === 3 ? 0 : lastVoteIndex + 1;
+
+					return {
+						day,
+						habit_id: habit.id,
+						vote: [...Object.keys(HABIT_VOTE_TYPES), null][voteIndex],
+					};
+				});
 
 				await HabitVote.createMany(payload);
 			}
