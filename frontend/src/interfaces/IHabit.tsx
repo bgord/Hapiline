973563f47e8de140cb402bc773dec2f832b8131b
Bartosz Gordon <bartosz.gordon@sealcode.org>
2019-12-31 08:25:10 +0100

    Make habit score and strengths types more expressive

--- frontend/src/interfaces/IHabit.tsx
+++ frontend/src/interfaces/IHabit.tsx
@@ -1,23 +1,23 @@
-type Score = "positive" | "neutral" | "negative";
-type Strength = "established" | "developing" | "fresh";
+type HabitScore = "positive" | "neutral" | "negative";
+type HabitStrength = "established" | "developing" | "fresh";
 
 export interface IHabit {
 	id: number;
 	name: string;
-	score: Score;
-	strength: Strength;
+	score: HabitScore;
+	strength: HabitStrength;
 	created_at: string;
 	updated_at: string;
 	order: number;
 }
 
-export const scoreToBgColor: {[key in Score]: string} = {
+export const scoreToBgColor: {[key in HabitScore]: string} = {
 	positive: "bg-green-300",
 	neutral: "bg-gray-300",
 	negative: "bg-red-300",
 };
 
-export const strengthToBgColor: {[key in Strength]: string} = {
+export const strengthToBgColor: {[key in HabitStrength]: string} = {
 	established: "bg-blue-300",
 	developing: "bg-blue-200",
 	fresh: "bg-blue-100",
