d0ccd747b91d9d284abcf7c5a7d914b50b874c20
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-01-02 15:13:02 +0100

    Display basic chart

--- frontend/src/HabitCharts.tsx
+++ frontend/src/HabitCharts.tsx
@@ -1,7 +1,9 @@
+import {format} from "date-fns";
 import * as Async from "react-async";
 import React from "react";
 
 import {IHabit} from "./interfaces/IHabit";
+import {Vote} from "./interfaces/IDayVote";
 import {api} from "./services/api";
 import {useErrorNotification} from "./contexts/notifications-context";
 
@@ -11,6 +13,12 @@ const chartRanges: {[key in ChartRange]: string} = {
 	last_week: "last_week",
 	last_month: "last_month",
 	all_time: "all_time",
+};
+
+const voteToBgColor: {[key in NonNullable<Vote>]: string} = {
+	progress: "bg-green-300",
+	plateau: "bg-gray-300",
+	regress: "bg-red-300",
 };
 
 export const HabitCharts: React.FC<{id: IHabit["id"]}> = ({id}) => {
@@ -25,10 +33,13 @@ export const HabitCharts: React.FC<{id: IHabit["id"]}> = ({id}) => {
 	});
 
 	return (
-		<div className="mt-6" style={{gridColumn: "span 4", gridRow: 4}}>
+		<div
+			className="mt-6 flex items-center"
+			style={{gridColumn: "span 4", gridRow: 4, alignSelf: "start"}}
+		>
 			<label>Select date range:</label>
 			<select
-				className="field ml-2"
+				className="field mx-2"
 				value={dateRange}
 				onChange={event => {
 					const {value} = event.target;
@@ -41,6 +52,13 @@ export const HabitCharts: React.FC<{id: IHabit["id"]}> = ({id}) => {
 				<option value="last_month">Last month</option>
 				<option value="all_time">All time</option>
 			</select>
+			{habitVoteChartRequestState.data?.map(item => (
+				<div
+					title={`${format(new Date(item.day), "yyyy-MM-dd")} - ${item.vote ?? "no vote"}`}
+					key={item.day}
+					className={`w-20 h-8 border-r-2 border-gray-500 ${voteToBgColor[item.vote ?? "plateau"]}`}
+				/>
+			))}
 		</div>
 	);
 };
