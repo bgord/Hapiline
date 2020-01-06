9b2881253e2e89fa10ccff1ec2a97c46759db6dc
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-01-06 10:54:23 +0100

    Improve HabitScore component

--- frontend/src/HabitScore.tsx
+++ frontend/src/HabitScore.tsx
@@ -6,12 +6,8 @@ export const HabitScore: React.FC<{score: IHabit["score"]} & JSX.IntrinsicElemen
 	score,
 	className = "",
 	...rest
-}) => {
-	const scoreBgColor = scoreToBgColor[score];
-	console.log(className);
-	return (
-		<div className={`${scoreBgColor} w-24 pl-1 p-2 text-center ${className}`} {...rest}>
-			{score}
-		</div>
-	);
-};
+}) => (
+	<div className={`${scoreToBgColor[score]} w-24 pl-1 p-2 text-center ${className}`} {...rest}>
+		{score}
+	</div>
+);
