c53c46eb4fa86ef19aea93ac52fb2ca1fb949554
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-01-06 16:07:57 +0100

    Add a script to backup db

---
+++ scripts/backup_db.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+TIMESTAMP=$(date +"%s")
+
+FILENAME="hapiline_backup_$TIMESTAMP"
+
+echo "$FILENAME"
+
+docker-compose exec db pg_dump -U docker -w -d hapiline -f "$FILENAME"
+docker-compose exec db mv "$FILENAME" "data/db"
