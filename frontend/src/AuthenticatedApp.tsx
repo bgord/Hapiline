e32e1088af704d9f9ff48f771409d96c6cf8876d
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-01-15 22:34:04 +0100

    Add new dashboard placeholder

--- frontend/src/AuthenticatedApp.tsx
+++ frontend/src/AuthenticatedApp.tsx
@@ -4,8 +4,9 @@ import * as React from "react";
 import {createBrowserHistory} from "history";
 
 import {Calendar} from "./Calendar";
-import {HabitsWindow} from "./HabitsWindow";
+import {DashboardWindow} from "./DashboardWindow";
 import {HabitsProvider} from "./contexts/habits-context";
+import {HabitsWindow} from "./HabitsWindow";
 import {Logo} from "./Logo";
 import {Logout} from "./Logout";
 import {Notifications} from "./Notifications";
@@ -29,7 +30,10 @@ function AuthenticatedApp() {
 					<Route exact path="/calendar">
 						<Calendar />
 					</Route>
-					<Redirect to="/habits" />
+					<Route exact path="/dashboard">
+						<DashboardWindow />
+					</Route>
+					<Redirect to="/dashboard" />
 				</Switch>
 			</Router>
 		</HabitsProvider>
@@ -42,8 +46,11 @@ function AuthenticatedNavbar() {
 	const [profile] = useUserProfile();
 	return (
 		<nav className="flex justify-end py-1 bg-white shadow-md">
-			<NavLink className="ml-2 mr-auto p-2" exact activeClassName="text-blue-400" to="/habits">
+			<NavLink className="ml-2 mr-auto p-2" exact activeClassName="text-blue-400" to="/dashboard">
 				<Logo />
+			</NavLink>
+			<NavLink exact className="p-4" activeClassName="text-blue-400" to="/dashboard">
+				Dashboard
 			</NavLink>
 			<NavLink exact className="p-4" activeClassName="text-blue-400" to="/habits">
 				Habits
---
+++ frontend/src/DashboardWindow.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const DashboardWindow = () => {
+	return <div>Brand new dashboard!</div>;
+};
