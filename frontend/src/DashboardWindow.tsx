56b4e560013ff29c080dd74e36c149010282ba2f
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-01-16 22:55:28 +0100

    Add a view today dashboard button

--- frontend/src/DashboardWindow.tsx
+++ frontend/src/DashboardWindow.tsx
@@ -1,5 +1,26 @@
+import {format} from "date-fns";
+import {useHistory} from "react-router-dom";
 import React from "react";
 
+import {BareButton} from "./BareButton";
+
 export const DashboardWindow = () => {
-	return <div>Brand new dashboard!</div>;
+	const history = useHistory();
+
+	const today = new Date();
+	const currentDate = format(today, "yyyy-MM-dd");
+
+	return (
+		<section className="flex max-w-2xl mx-auto mt-12">
+			<header className="flex w-full">
+				<h1 className="text-xl font-bold">Hello!</h1>
+				<BareButton
+					onClick={() => history.push(`/calendar?previewDay=${currentDate}`)}
+					className="ml-auto bg-blue-300"
+				>
+					View today
+				</BareButton>
+			</header>
+		</section>
+	);
 };
