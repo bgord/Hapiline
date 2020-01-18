0fcfeb18a2ede6e280b35a71dcc332573d17e06c
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-01-18 13:55:26 +0100

    Create formatter functions

--- frontend/src/config/DATE_FORMATS.js
+++
@@ -1,5 +0,0 @@
-export const DATE_FORMATS = {
-	day: "yyyy-MM-dd",
-	time: "yyyy-MM-dd HH:mm",
-	month: "MMMM yyyy",
-};
---
+++ frontend/src/config/DATE_FORMATS.ts
@@ -0,0 +1,11 @@
+import {format} from "date-fns";
+
+const DATE_FORMATS = {
+	day: "yyyy-MM-dd",
+	time: "yyyy-MM-dd HH:mm",
+	month: "MMMM yyyy",
+};
+
+export const formatDay = (value: number | Date): string => format(value, DATE_FORMATS.day);
+export const formatTime = (value: number | Date): string => format(value, DATE_FORMATS.time);
+export const formatMonth = (value: number | Date): string => format(value, DATE_FORMATS.month);
