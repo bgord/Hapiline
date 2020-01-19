fb4c73505f2a7a6b617ee633c73a0a466252f0aa
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-01-19 19:12:53 +0100

    Extract comment to a separate component

--- frontend/src/HabitVoteCommentHistory.tsx
+++ frontend/src/HabitVoteCommentHistory.tsx
@@ -4,12 +4,12 @@ import React from "react";
 
 import {ErrorMessage} from "./ErrorMessages";
 import {IHabit} from "./interfaces/IHabit";
+import {IVoteComment, voteToBgColor} from "./interfaces/IDayVote";
 import {InfoMessage} from "./InfoMessage";
 import {api} from "./services/api";
 import {constructUrl} from "./hooks/useQueryParam";
 import {formatDay, formatDayName} from "./config/DATE_FORMATS";
 import {useErrorNotification} from "./contexts/notifications-context";
-import {voteToBgColor} from "./interfaces/IDayVote";
 
 export const HabitVoteCommentHistory: React.FC<{habitId: IHabit["id"]}> = ({habitId}) => {
 	const triggerErrorNotification = useErrorNotification();
@@ -35,27 +35,37 @@ export const HabitVoteCommentHistory: React.FC<{habitId: IHabit["id"]}> = ({habi
 				{voteComments.length > 0 && (
 					<ul className="my-8">
 						{voteComments.map(voteComment => (
-							<li key={voteComment.id} className="flex flex-col mb-4">
-								<div className="flex">
-									<strong>
-										{formatDay(voteComment.day)} ({formatDayName(voteComment.day)})
-									</strong>
-									<Link
-										to={constructUrl("calendar", {
-											preview_day: formatDay(voteComment.day),
-											highlighted_habit_id: voteComment?.habit_id?.toString(),
-										})}
-										className={`${voteToBgColor.get(voteComment.vote)} px-2 ml-4`}
-									>
-										{voteComment.vote?.toUpperCase() ?? "NO VOTE"}
-									</Link>
-								</div>
-								<textarea value={voteComment.comment} disabled className="w-full border p-1 mt-1" />
-							</li>
+							<HabitVoteComment {...voteComment} />
 						))}
 					</ul>
 				)}
 			</Async.IfFulfilled>
 		</div>
+	);
+};
+
+const HabitVoteComment: React.FC<IVoteComment> = ({id, day, habit_id, vote, comment}) => {
+	const voteUrl = constructUrl("calendar", {
+		preview_day: formatDay(day),
+		highlighted_habit_id: habit_id?.toString(),
+	});
+
+	const formattedDay = formatDay(day);
+	const formattedDayName = formatDayName(day);
+
+	const linkBgColor = voteToBgColor.get(vote);
+
+	return (
+		<li key={id} className="flex flex-col mb-4">
+			<div className="flex">
+				<strong>
+					{formattedDay} ({formattedDayName})
+				</strong>
+				<Link to={voteUrl} className={`${linkBgColor} px-2 ml-4`}>
+					{vote?.toUpperCase() ?? "NO VOTE"}
+				</Link>
+			</div>
+			<textarea value={comment} disabled className="w-full border p-1 mt-1" />
+		</li>
 	);
 };
