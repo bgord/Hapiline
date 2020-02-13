40233db12f56ba0fd3f6aad0e3820b0ae9066597
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-02-13 20:50:05 +0100

    Improve Demo decorator component

--- __fixtures__/_Demo.tsx
+++ __fixtures__/_Demo.tsx
@@ -2,6 +2,6 @@ import "../frontend/css/index.css";
 
 import React from "react";
 
-export const Demo: React.FC = ({children}) => {
-	return <>{children}</>;
-};
+export const Demo: React.FC = ({children}) => (
+	<div className="w-screen h-screen flex justify-center p-4">{children}</div>
+);
