2fbb8027c11070da16ce37dacb9951675e505e4d
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-02-17 20:25:47 +0100

    Add Header UI variants

--- frontend/src/ui/header/Header.tsx
+++ frontend/src/ui/header/Header.tsx
@@ -2,6 +2,10 @@ import "./header.css";
 
 import React from "react";
 
-export const Header: React.FC<JSX.IntrinsicElements["header"]> = props => (
-	<header className="c-header" {...props} />
+type HeaderVariant = "small" | "medium" | "large";
+
+export const Header: React.FC<JSX.IntrinsicElements["header"] & {
+	variant?: HeaderVariant;
+}> = ({variant = "medium", ...props}) => (
+	<header className={`c-header c-header--${variant}`} {...props} />
 );
