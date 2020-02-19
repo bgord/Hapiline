bbd67afec34c8a9497912a03687e4ab4b18602e0
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-02-19 21:45:02 +0100

    Implement Radio UI component

--- __fixtures__/Radio.tsx
+++ __fixtures__/Radio.tsx
@@ -10,14 +10,19 @@ export default {
 		<Demo>
 			<Field variant="row">
 				<Radio id="left" name="direction" value="left" />
-				<Label htmlFor="left" style={{marginLeft: "2px"}}>
+				<Label htmlFor="left" style={{marginLeft: "4px"}}>
 					Left
 				</Label>
 			</Field>
-
+			<Field variant="row" style={{marginLeft: "24px"}}>
+				<Radio id="medium" name="direction" value="medium" disabled />
+				<Label htmlFor="medium" style={{marginLeft: "4px"}}>
+					Medium
+				</Label>
+			</Field>
 			<Field variant="row" style={{marginLeft: "24px"}}>
 				<Radio id="right" name="direction" value="right" />
-				<Label htmlFor="right" style={{marginLeft: "2px"}}>
+				<Label htmlFor="right" style={{marginLeft: "4px"}}>
 					Right
 				</Label>
 			</Field>
