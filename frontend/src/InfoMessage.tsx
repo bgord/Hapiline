c13108e104476f757a2a9f01477bd09f5552a1e7
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-02-22 19:38:13 +0100

    Remove redundant component

--- frontend/src/InfoMessage.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const InfoMessage: React.FC<JSX.IntrinsicElements["div"]> = ({className = "", ...props}) => {
-	return <div className={`text-center ${className}`} {...props} />;
-};
--- frontend/src/SuccessMessages.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export const SuccessMessage: React.FC<JSX.IntrinsicElements["div"]> = ({
-	className = "",
-	...props
-}) => (
-	<div className={`success-message w-auto flex justify-between mt-4 ${className}`} {...props} />
-);
