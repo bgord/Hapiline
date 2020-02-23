ef62bcd5edf5fa8d889c3490b5eec2a095b32c5c
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-02-23 14:34:54 +0100

    Adjust NewPassword to vertical rhythm

--- frontend/src/NewPasswordWindow.tsx
+++ frontend/src/NewPasswordWindow.tsx
@@ -3,6 +3,8 @@ import * as Async from "react-async";
 import React from "react";
 
 import {Button} from "./ui/button/Button";
+import {Card} from "./ui/card/Card";
+import {Column} from "./ui/column/Column";
 import {Field} from "./ui/field/Field";
 import {Header} from "./ui/header/Header";
 import {Input} from "./ui/input/Input";
@@ -24,67 +26,69 @@ export const NewPasswordWindow: React.FC = () => {
 	const {errorMessage} = getRequestStateErrors(newPasswordRequestState);
 
 	return (
-		<div className="bg-white rounded shadow-lg p-8 md:max-w-sm md:mx-auto ">
+		<Card pt="48" pb="48" pr="24" pl="24" ml="auto" mr="auto" mt="72">
 			<form
 				onSubmit={(event: React.FormEvent<HTMLFormElement>) => {
 					event.preventDefault();
 					newPasswordRequestState.run(token, password, passwordConfirmation);
 				}}
-				className="mb-4 md:flex md:flex-wrap md:justify-between"
 			>
-				<Header>New password</Header>
-				<Field mt="48" width="100%">
-					<Label htmlFor="password">Password</Label>
-					<Input
-						id="password"
-						placeholder="********"
-						autoComplete="new-password"
-						title="Password should contain at least 6 characters."
-						value={password}
-						onChange={event => setPassword(event.target.value)}
-						type="password"
-						required
-						pattern=".{6,}"
-						disabled={newPasswordRequestState.isFulfilled}
-					/>
-				</Field>
-				<Field width="100%" mt="12" mb="24">
-					<Label htmlFor="password_confirmation">Repeat password</Label>
-					<Input
-						id="password_confirmation"
-						type="password"
-						placeholder="********"
-						pattern={password}
-						title="Passwords have to be equal"
-						value={passwordConfirmation}
-						onChange={event => setPasswordConfirmation(event.target.value)}
-						required
-						disabled={newPasswordRequestState.isFulfilled}
-					/>
-				</Field>
-				<Row mainAxis="end">
-					<Button
-						variant="primary"
-						type="submit"
-						disabled={newPasswordRequestState.isFulfilled}
-						data-testid="registration-submit"
-					>
-						{newPasswordRequestState.isPending ? "Loading..." : "Change password"}
-					</Button>
-				</Row>
-				<Async.IfFulfilled state={newPasswordRequestState}>
-					<Text>Password has been changed!</Text>
-					<div className="flex mt-4">
-						<span className="text-sm">You can </span>
-						<Link className="link ml-1" to="/login">
-							login now
-						</Link>
-					</div>
-				</Async.IfFulfilled>
-				<Async.IfRejected state={newPasswordRequestState}>
-					<RequestErrorMessage>{errorMessage}</RequestErrorMessage>
-				</Async.IfRejected>
+				<Column>
+					<Header>New password</Header>
+					<Field mt="48">
+						<Label htmlFor="password">Password</Label>
+						<Input
+							id="password"
+							placeholder="********"
+							autoComplete="new-password"
+							title="Password should contain at least 6 characters."
+							value={password}
+							onChange={event => setPassword(event.target.value)}
+							type="password"
+							required
+							pattern=".{6,}"
+							disabled={newPasswordRequestState.isFulfilled}
+							style={{width: "500px"}}
+						/>
+					</Field>
+					<Field mt="12">
+						<Label htmlFor="password_confirmation">Repeat password</Label>
+						<Input
+							id="password_confirmation"
+							type="password"
+							placeholder="********"
+							pattern={password}
+							title="Passwords have to be equal"
+							value={passwordConfirmation}
+							onChange={event => setPasswordConfirmation(event.target.value)}
+							required
+							disabled={newPasswordRequestState.isFulfilled}
+						/>
+					</Field>
+					<Row mainAxis="end" mt="24">
+						<Button
+							variant="primary"
+							type="submit"
+							disabled={newPasswordRequestState.isFulfilled}
+							data-testid="registration-submit"
+						>
+							{newPasswordRequestState.isPending ? "Loading..." : "Change password"}
+						</Button>
+					</Row>
+					<Async.IfFulfilled state={newPasswordRequestState}>
+						<Text>Password has been changed!</Text>
+						<Row mt="12">
+							<span className="text-sm">You can </span>
+							<Link className="link ml-1" to="/login">
+								login now
+							</Link>
+						</Row>
+					</Async.IfFulfilled>
+					<Async.IfRejected state={newPasswordRequestState}>
+						<RequestErrorMessage>{errorMessage}</RequestErrorMessage>
+					</Async.IfRejected>
+				</Column>
 			</form>
-		</div>
+		</Card>
 	);
 };
