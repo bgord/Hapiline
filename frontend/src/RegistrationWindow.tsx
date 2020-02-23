b5ed76a648378bccc3e5b135878947f1f4185f8f
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-02-23 14:38:33 +0100

    Adjust Registration to vertical rhythm

--- frontend/src/RegistrationWindow.tsx
+++ frontend/src/RegistrationWindow.tsx
@@ -3,6 +3,8 @@ import * as Async from "react-async";
 import React from "react";
 
 import {Button} from "./ui/button/Button";
+import {Card} from "./ui/card/Card";
+import {Column} from "./ui/column/Column";
 import {ErrorMessage, RequestErrorMessage} from "./ErrorMessages";
 import {Field} from "./ui/field/Field";
 import {Header} from "./ui/header/Header";
@@ -27,82 +29,84 @@ export const RegistrationWindow: React.FC = () => {
 	const emailInlineErrorMessage = getArgErrorMessage("email");
 
 	return (
-		<div className="bg-white rounded shadow-lg p-8 md:max-w-sm md:mx-auto ">
+		<Card pt="48" pb="48" pr="24" pl="24" ml="auto" mr="auto" mt="72">
 			<form
 				onSubmit={(event: React.FormEvent<HTMLFormElement>) => {
 					event.preventDefault();
 					registrationRequestState.run(email, password, passwordConfirmation);
 				}}
-				className="mb-4 md:flex md:flex-wrap md:justify-between"
 			>
-				<Header>Register</Header>
-				<Field width="100%" mt="48">
-					<Label htmlFor="email">Email</Label>
-					<Input
-						id="email"
-						value={email}
-						onChange={event => setEmail(event.target.value)}
-						required
-						type="email"
-						disabled={registrationRequestState.isFulfilled}
-						placeholder="john.brown@gmail.com"
-					/>
-					<Async.IfRejected state={registrationRequestState}>
-						<ErrorMessage>{emailInlineErrorMessage}</ErrorMessage>
-					</Async.IfRejected>
-				</Field>
-				<Field width="100%" mt="12">
-					<Label htmlFor="password">Password</Label>
-					<Input
-						id="password"
-						placeholder="********"
-						title="Password should contain at least 6 characters."
-						value={password}
-						onChange={event => setPassword(event.target.value)}
-						type="password"
-						required
-						pattern=".{6,}"
-						disabled={registrationRequestState.isFulfilled}
-					/>
-				</Field>
-				<Field width="100%" mt="12">
-					<Label htmlFor="password_confirmation">Repeat password</Label>
-					<Input
-						id="password_confirmation"
-						type="password"
-						placeholder="********"
-						pattern={password}
-						required
-						title="Passwords have to be equal"
-						value={passwordConfirmation}
-						onChange={event => setPasswordConfirmation(event.target.value)}
-						disabled={registrationRequestState.isFulfilled}
-					/>
-				</Field>
-				<Row mt="24" mainAxis="end">
-					<Button
-						data-testid="registration-submit"
-						type="submit"
-						variant="primary"
-						disabled={registrationRequestState.isFulfilled}
-						style={{width: "125px"}}
-					>
-						{registrationRequestState.isPending ? "Loading..." : "Register"}
-					</Button>
-				</Row>
-				<Async.IfFulfilled state={registrationRequestState}>
-					<Text>Account confirmation email has been sent!</Text>
-					<Row mt="24">
-						<span className="text-sm">You can </span>
-						<Link className="link ml-1" to="/login">
-							login now
-						</Link>
+				<Column>
+					<Header>Register</Header>
+					<Field mt="48">
+						<Label htmlFor="email">Email</Label>
+						<Input
+							id="email"
+							value={email}
+							onChange={event => setEmail(event.target.value)}
+							required
+							type="email"
+							disabled={registrationRequestState.isFulfilled}
+							placeholder="john.brown@gmail.com"
+							style={{width: "500px"}}
+						/>
+						<Async.IfRejected state={registrationRequestState}>
+							<ErrorMessage>{emailInlineErrorMessage}</ErrorMessage>
+						</Async.IfRejected>
+					</Field>
+					<Field mt="12">
+						<Label htmlFor="password">Password</Label>
+						<Input
+							id="password"
+							placeholder="********"
+							title="Password should contain at least 6 characters."
+							value={password}
+							onChange={event => setPassword(event.target.value)}
+							type="password"
+							required
+							pattern=".{6,}"
+							disabled={registrationRequestState.isFulfilled}
+						/>
+					</Field>
+					<Field mt="12">
+						<Label htmlFor="password_confirmation">Repeat password</Label>
+						<Input
+							id="password_confirmation"
+							type="password"
+							placeholder="********"
+							pattern={password}
+							required
+							title="Passwords have to be equal"
+							value={passwordConfirmation}
+							onChange={event => setPasswordConfirmation(event.target.value)}
+							disabled={registrationRequestState.isFulfilled}
+						/>
+					</Field>
+					<Row mt="24" mainAxis="end">
+						<Button
+							data-testid="registration-submit"
+							type="submit"
+							variant="primary"
+							disabled={registrationRequestState.isFulfilled}
+							style={{width: "125px"}}
+						>
+							{registrationRequestState.isPending ? "Loading..." : "Register"}
+						</Button>
 					</Row>
-				</Async.IfFulfilled>
-				<Async.IfRejected state={registrationRequestState}>
-					<RequestErrorMessage>{responseStatus === 500 && errorMessage}</RequestErrorMessage>
-				</Async.IfRejected>
+					<Async.IfFulfilled state={registrationRequestState}>
+						<Text>Account confirmation email has been sent!</Text>
+						<Row mt="24">
+							<span className="text-sm">You can </span>
+							<Link className="link ml-1" to="/login">
+								login now
+							</Link>
+						</Row>
+					</Async.IfFulfilled>
+					<Async.IfRejected state={registrationRequestState}>
+						<RequestErrorMessage>{responseStatus === 500 && errorMessage}</RequestErrorMessage>
+					</Async.IfRejected>
+				</Column>
 			</form>
-		</div>
+		</Card>
 	);
 };
