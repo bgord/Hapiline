77f51f7cc76fbbea275d0b12725a6d82ded39d77
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-03-06 10:21:11 +0100

    Use predefined spacing variables wherever possible

--- __fixtures__/_Demo.tsx
+++ __fixtures__/_Demo.tsx
@@ -1,17 +1,9 @@
 import "../frontend/css/main.css";
 import React from "react";
+import {Row} from "../frontend/src/ui/row/Row";
 
 export const Demo: React.FC = ({children}) => (
-	<div
-		style={{
-			display: "flex",
-			alignItems: "start",
-			width: "100vh",
-			height: "100vh",
-			paddingTop: "24px",
-			paddingLeft: "24px",
-		}}
-	>
+	<Row pt="24" pl="24" crossAxis="start" style={{width: "100vh", height: "100vh"}}>
 		{children}
-	</div>
+	</Row>
 );
