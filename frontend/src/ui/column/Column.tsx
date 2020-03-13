d48e9cd8f66093d112b611cec64e68056d013c72
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-03-13 22:39:12 +0100

    Refactor Column to accept as prop

--- frontend/src/ui/column/Column.tsx
+++ frontend/src/ui/column/Column.tsx
@@ -1,6 +1,7 @@
 import "./column.css";
 
 import React from "react";
+import {Box, PolymorphicComponentProps} from "react-polymorphic-box";
 
 import {Alignments} from "../alignments";
 import {Margins} from "../margins";
@@ -8,61 +9,63 @@ import {Paddings} from "../paddings";
 import {Widths} from "../widths";
 import {Positions} from "../positions";
 
-type ColumnProps = React.ComponentPropsWithoutRef<"div"> &
-	Margins &
-	Alignments &
-	Paddings &
-	Widths &
-	Positions;
+type ColumnOwnProps = Margins & Alignments & Paddings & Widths & Positions;
 
-/* eslint-disable prefer-arrow-callback */
-export const Column = React.forwardRef(function _Column(
-	{
-		m,
-		mx,
-		my,
-		mt,
-		mr,
-		mb,
-		ml,
-		p,
-		px,
-		py,
-		pt,
-		pr,
-		pb,
-		pl,
-		mainAxis,
-		crossAxis,
-		width,
-		position = "static",
-		...props
-	}: ColumnProps,
-	ref: React.Ref<HTMLDivElement>,
-) {
-	return (
-		<div
-			ref={ref}
-			data-main-axis={mainAxis}
-			data-cross-axis={crossAxis}
-			data-m={m}
-			data-mx={mx}
-			data-my={my}
-			data-mt={mt}
-			data-mr={mr}
-			data-mb={mb}
-			data-ml={ml}
-			data-p={p}
-			data-px={px}
-			data-py={py}
-			data-pt={pt}
-			data-pr={pr}
-			data-pb={pb}
-			data-pl={pl}
-			data-width={width}
-			data-position={position}
-			className="c-column"
-			{...props}
-		/>
-	);
-});
+export type ColumnProps<E extends React.ElementType> = PolymorphicComponentProps<E, ColumnOwnProps>;
+
+const defaultElement = "div";
+
+export const Column = React.forwardRef(
+	<E extends React.ElementType = typeof defaultElement>(
+		{
+			ref,
+			m,
+			mx,
+			my,
+			mt,
+			mr,
+			mb,
+			ml,
+			p,
+			px,
+			py,
+			pt,
+			pr,
+			pb,
+			pl,
+			mainAxis,
+			crossAxis,
+			width,
+			position = "static",
+			...props
+		}: ColumnProps<E>,
+		innerRef: typeof ref,
+	) => {
+		return (
+			<Box
+				ref={innerRef}
+				as={defaultElement}
+				data-main-axis={mainAxis}
+				data-cross-axis={crossAxis}
+				data-m={m}
+				data-mx={mx}
+				data-my={my}
+				data-mt={mt}
+				data-mr={mr}
+				data-mb={mb}
+				data-ml={ml}
+				data-p={p}
+				data-px={px}
+				data-py={py}
+				data-pt={pt}
+				data-pr={pr}
+				data-pb={pb}
+				data-pl={pl}
+				data-width={width}
+				data-position={position}
+				className="c-column"
+				{...props}
+			/>
+		);
+	},
+) as <E extends React.ElementType = typeof defaultElement>(props: ColumnProps<E>) => JSX.Element;
