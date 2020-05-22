b2263e4673fc8765c5496a1bbe40fa2218a8326e
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-05-22 23:20:06 +0200

    Add a directory for backups on VPS

--- scripts/backup_db.sh
+++ scripts/backup_db.sh
@@ -7,4 +7,4 @@ FILENAME="hapiline_backup_$TIMESTAMP"
 echo "$FILENAME"
 
 docker-compose exec db pg_dump --data-only -U docker -w -d hapiline -f "$FILENAME"
-docker-compose exec db mv "$FILENAME" "data/db"
+docker-compose exec db mv "$FILENAME" "/backups"
