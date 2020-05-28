61a496d7990e6f019015b000a9530ad82a88f74e
Bartosz Gordon <bgord@users.noreply.github.com>
2020-05-28 22:07:01 +0200

    Improve streaks (#68)
    * Setup unit tests
    
    * Document the basic streak calculator
    
    * Make null vote only today not reset the streak
    
    * Make single null or plateau don't break the streak
    
    * Fix registration window
    
    * Improve docs
    
    Co-authored-by: Bartosz Gordon <bartosz.gordon@sealcode.org>

--- app/Beings/VotesStreakCalculator.js
+++ app/Beings/VotesStreakCalculator.js
@@ -2,18 +2,50 @@ const HABIT_VOTE_TYPES = use("HABIT_VOTE_TYPES");
 
 class VotesStreakCalculator {
 	constructor(votes) {
-		this.votes = votes.map(entry => entry.vote);
+		// It accepts the votes that are in reverse chronological order
+		// but under the hood a conversion is made to make calculations easier.
+		this.votes = [...votes].map(entry => entry.vote).reverse();
 	}
 
 	calculate(type) {
+		// By default, there's no streak
 		let streak = 0;
 
+		// The votes array looks like that (sorted by day)
+		// [
+		//  { day: 2020-05-27T00:00:00.000Z, vote: 'progress'},
+		//  { day: 2020-05-28T00:00:00.000Z, vote: null},
+		//  ...
+		// ]
+
+		// The algorithm:
+		// - given streak counts if there are only votes of given type in a row,
+		//   without interruptions
+		//
+		// - `null` and `plateau` votes reset the streaks with a few exceptions:
+		//    - if today's vote is `null` and the previous vote was correct
+		//    - PROGRESS null PROGRESS
+		//    - PROGRESS PLATEAU PROGRESS
+		//
 		for (const [index, vote] of this.votes.entries()) {
-			if (index === 0 && vote !== HABIT_VOTE_TYPES[type]) {
-				break;
-			} else if (vote === HABIT_VOTE_TYPES[type]) {
+			// If today's vote is `null`, it doesn't reset the streak
+			const isNullVoteForToday =
+				index === this.votes.length - 1 && vote === null && this.votes[index - 1] === type;
+
+			// Don't break PROGRESS null PROGRESS streak
+			const nullIsAfterDesiredType = vote === null && this.votes[index - 1] === type;
+
+			// Don't break PROGRESS PLATEAU PROGRESS streak
+			const plateauIsAfterDesiredType =
+				vote === HABIT_VOTE_TYPES.plateau && this.votes[index - 1] === type;
+
+			if (vote === HABIT_VOTE_TYPES[type]) {
 				streak++;
-			} else break;
+			} else if (isNullVoteForToday || nullIsAfterDesiredType || plateauIsAfterDesiredType) {
+				continue;
+			} else {
+				streak = 0;
+			}
 		}
 
 		return streak;
---
+++ test/unit/votes-streak-calculator.spec.js
@@ -0,0 +1,137 @@
+const {test} = use("Test/Suite")("VotesStreakCalculator");
+const {VotesStreakCalculator} = require("../../app/Beings/VotesStreakCalculator");
+const dateFns = require("date-fns");
+
+const HABIT_VOTE_TYPES = use("HABIT_VOTE_TYPES");
+
+test("it works as expected", async ({assert}) => {
+	const cases = [
+		// Votes are for: today, yesterday, day before yesterday, etc...
+		[[null, null, null], 0, HABIT_VOTE_TYPES.progress],
+
+		// Basic 3 straight days
+		[
+			[HABIT_VOTE_TYPES.progress, HABIT_VOTE_TYPES.progress, HABIT_VOTE_TYPES.progress],
+			3,
+			HABIT_VOTE_TYPES.progress,
+		],
+
+		// Check if it doesn't return same results for both regress and progress
+		[
+			[HABIT_VOTE_TYPES.progress, HABIT_VOTE_TYPES.progress, HABIT_VOTE_TYPES.progress],
+			0,
+			HABIT_VOTE_TYPES.regress,
+		],
+
+		// `null` vote resets the streak if today has a non-null breaking vote
+		[
+			[HABIT_VOTE_TYPES.regress, null, HABIT_VOTE_TYPES.progress, HABIT_VOTE_TYPES.progress],
+			0,
+			HABIT_VOTE_TYPES.progress,
+		],
+
+		// `plateau` resets the streak
+		[
+			[
+				HABIT_VOTE_TYPES.progress,
+				HABIT_VOTE_TYPES.plateau,
+				HABIT_VOTE_TYPES.plateau,
+				HABIT_VOTE_TYPES.plateau,
+			],
+			1,
+			HABIT_VOTE_TYPES.progress,
+		],
+
+		// `null` doesn't reset the streak because today has `null` vote
+		[[null, HABIT_VOTE_TYPES.progress, HABIT_VOTE_TYPES.progress], 2, HABIT_VOTE_TYPES.progress],
+
+		// two `null`s reset the streak
+		[
+			[null, null, HABIT_VOTE_TYPES.progress, HABIT_VOTE_TYPES.progress],
+			0,
+			HABIT_VOTE_TYPES.progress,
+		],
+
+		// One `null` in between desired values doesn't break the streak
+		[
+			[HABIT_VOTE_TYPES.progress, null, HABIT_VOTE_TYPES.progress, HABIT_VOTE_TYPES.progress],
+			3,
+			HABIT_VOTE_TYPES.progress,
+		],
+
+		// Two `null`s in between desired values break the streak
+		[
+			[HABIT_VOTE_TYPES.progress, null, null, HABIT_VOTE_TYPES.progress],
+			1,
+			HABIT_VOTE_TYPES.progress,
+		],
+
+		// Two `null`s in between desired values break the streak
+		[
+			[HABIT_VOTE_TYPES.regress, null, null, HABIT_VOTE_TYPES.progress],
+			0,
+			HABIT_VOTE_TYPES.progress,
+		],
+
+		// One `plateau` in between desired values doesn't break the streak
+		[
+			[
+				HABIT_VOTE_TYPES.progress,
+				HABIT_VOTE_TYPES.plateau,
+				HABIT_VOTE_TYPES.progress,
+				HABIT_VOTE_TYPES.progress,
+			],
+			3,
+			HABIT_VOTE_TYPES.progress,
+		],
+
+		// Two `plateau`s in between desired values break the streak
+		[
+			[
+				HABIT_VOTE_TYPES.progress,
+				HABIT_VOTE_TYPES.plateau,
+				HABIT_VOTE_TYPES.plateau,
+				HABIT_VOTE_TYPES.progress,
+			],
+			1,
+			HABIT_VOTE_TYPES.progress,
+		],
+
+		// Two `plateau`s in between desired values break the streak
+		[
+			[
+				HABIT_VOTE_TYPES.regress,
+				HABIT_VOTE_TYPES.plateau,
+				HABIT_VOTE_TYPES.plateau,
+				HABIT_VOTE_TYPES.progress,
+			],
+			0,
+			HABIT_VOTE_TYPES.progress,
+		],
+
+		[
+			[HABIT_VOTE_TYPES.regress, null, HABIT_VOTE_TYPES.plateau, HABIT_VOTE_TYPES.regress],
+			1,
+			HABIT_VOTE_TYPES.regress,
+		],
+
+		// `null` today doesn't break the streak if there's a correct vote the day before
+		[
+			[null, HABIT_VOTE_TYPES.plateau, HABIT_VOTE_TYPES.regress, HABIT_VOTE_TYPES.regress],
+			0,
+			HABIT_VOTE_TYPES.regress,
+		],
+	];
+
+	for (const [votes, expected, voteType] of cases) {
+		const payload = votes.map((vote, index) => ({
+			day: dateFns.subDays(new Date(), index),
+			vote,
+		}));
+
+		const streaksCalculator = new VotesStreakCalculator(payload);
+		const result = streaksCalculator.calculate(voteType);
+
+		assert.equal(result, expected);
+	}
+});
