fbead3f1d2d76820d3fba7ef1a840e6e90e4d9c1
Bartosz Gordon <bgord@users.noreply.github.com>
2020-05-29 09:57:04 +0200

    Don't say a full stringified date in the GET /day-votes URL (#72)
    Co-authored-by: Bartosz Gordon <bartosz.gordon@sealcode.org>

--- frontend/src/services/calendar.ts
+++ frontend/src/services/calendar.ts
@@ -2,6 +2,7 @@ import {_internal_api} from "./api";
 import {constructUrl} from "../hooks/useQueryParam";
 import {HabitVote, DayStatsFromServer} from "../interfaces/index";
 import {MonthOffset} from "../hooks/useMonthsWidget";
+import {formatDay} from "../config/DATE_FORMATS";
 
 export const getMonthRequest = (_key: "month", monthOffset: MonthOffset) =>
 	_internal_api
@@ -10,5 +11,5 @@ export const getMonthRequest = (_key: "month", monthOffset: MonthOffset) =>
 
 export const getDayRequest = (_key: "day", day: Date) =>
 	_internal_api
-		.get<HabitVote[]>(constructUrl("/day-votes", {day: String(day)}))
+		.get<HabitVote[]>(constructUrl("/day-votes", {day: formatDay(day)}))
 		.then(response => response.data);
