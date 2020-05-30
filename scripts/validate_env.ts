721ca27fa06e1125ce111d993e412567d3bea40e
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-05-30 10:58:30 +0200

    Load frontend env files

--- scripts/validate_env.ts
+++ scripts/validate_env.ts
@@ -1,5 +1,25 @@
+import dotenv from "dotenv";
+import fs from "fs";
+
+const envFrontendExampleFilename = ".env-frontend.example";
+const envFrontendProductionFilename = ".env-frontend.prod";
+
 async function main() {
-	console.log("Validating server ad frontend env variables ⌛");
+	console.log("⌛ Validating server and frontend env variables");
+
+	const envFrontendExampleString = await fs.promises.readFile(envFrontendExampleFilename, {
+		encoding: "utf8",
+	});
+	const envFrontendExample = dotenv.parse(envFrontendExampleString);
+	console.log(`\n✓ Loaded and parsed the example frontend env file: ${envFrontendExampleFilename}`);
+
+	const envFrontendProductionString = await fs.promises.readFile(envFrontendProductionFilename, {
+		encoding: "utf8",
+	});
+	const envFrontendProduction = dotenv.parse(envFrontendProductionString);
+	console.log(
+		`✓ Loaded and parsed the production frontend env file: ${envFrontendProductionFilename}`,
+	);
 }
 
 main();
