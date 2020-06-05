50cbe7170ce81d9b2f570bbc6c5c1a41aa8a65d5
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-06-05 19:59:32 +0200

    Fix habit seeds

--- database/seeds/3.HabitSeeder.js
+++ database/seeds/3.HabitSeeder.js
@@ -3,7 +3,6 @@ const datefns = require("date-fns");
 const Database = use("Database");
 const HABIT_SCORE_TYPES = use("HABIT_SCORE_TYPES");
 const HABIT_STRENGTH_TYPES = use("HABIT_STRENGTH_TYPES");
-const Habit = use("Habit");
 
 const today = Date.now();
 
@@ -16,7 +15,7 @@ class HabitsSeeder {
 
 			const payload = Array.from({length: howManyHabits}).map((_, index) => {
 				const ts = datefns.subDays(today, index % 3).setHours(index & 3);
-				const date = new Date(ts);
+				const date = new Date(ts).toISOString();
 
 				return {
 					user_id: user.id,
@@ -31,7 +30,9 @@ class HabitsSeeder {
 				};
 			});
 
-			await Habit.createMany(payload);
+			// Using Database.insert here because we want to override the
+			// default {created,updated}_at dates (which are set to "now").
+			await Database.into("habits").insert(payload);
 		}
 	}
 }
