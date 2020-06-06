f8655916e8708a45a71fcde1271d7cc477dc3f86
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-06-06 23:02:07 +0200

    Reorganize checks

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+printf "You're about to deploy Hapiline!\n\n"
+
+ALLOWED_BRANCH="master"
+CURRENT_BRANCH=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
+
+echo "Your current branch: $CURRENT_BRANCH"
+
+if [ $ALLOWED_BRANCH == "$CURRENT_BRANCH" ]; then
+  echo "Deployment is required from your branch, proceeding.";
+else
+  echo "The deployment script is required to be run on branch \`master\`";
+  echo "Quitting..."
+  exit 1
+fi
+
 printf '\nDeployment procedure will be started in 5 seconds.\n'
 printf 'Press Ctrl-C (or Command-C) is you want to quit.\n\n'
 
@@ -19,19 +34,6 @@ echo '1'
 sleep 1s
 
 printf '\nDeployment procedure started!\n\n'
-
-ALLOWED_BRANCH="master"
-CURRENT_BRANCH=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
-
-echo "Your current branch: $CURRENT_BRANCH"
-
-if [ $ALLOWED_BRANCH == "$CURRENT_BRANCH" ]; then
-  echo "Deployment is required from your branch, proceeding.";
-else
-  echo "Deployment is required to be run on branch \`master\`";
-  echo "Quitting..."
-  exit 1
-fi
 
 echo "Running backend tests..."
 
