f6360197e97fe79bc826ffd1319145ae0af6cec1
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-06-06 22:31:03 +0200

    Implement countdown

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
 
-echo 'Deployment procedure started!'
+printf '\nDeployment procedure will be started in 5 seconds.\n'
+printf 'Press Ctrl-C (or Command-C) is you want to quit.\n\n'
+
+echo '5'
+sleep 1s
+
+echo '4'
+sleep 1s
+
+echo '3'
+sleep 1s
+
+echo '2'
+sleep 1s
+
+echo '1'
+sleep 1s
+
+printf '\nDeployment procedure started!'
