448c4e048273ba14612376444b9a4125db486e4d
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-06-06 22:37:33 +0200

    Bailout when the current branch is not master

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,4 +18,18 @@ sleep 1s
 echo '1'
 sleep 1s
 
-printf '\nDeployment procedure started!'
+printf '\nDeployment procedure started!\n\n'
+
+ALLOWED_BRANCH="master"
+CURRENT_BRANCH=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
+
+echo "Your current branch: $CURRENT_BRANCH"
+
+if [ $ALLOWED_BRANCH == "$CURRENT_BRANCH" ]; then
+  echo "Deployment is required from your branch, proceeding.";
+else
+  echo "Deployment is required to be run on branch \`master\`";
+  echo "Quitting..."
+  exit 1
+fi
+
