98eccd603e1a1fa6c00180e9423844a83c756174
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-06-07 19:14:07 +0200

    Remove redundant optional chaining for timestamps

--- frontend/src/DayDialogSummary.tsx
+++ frontend/src/DayDialogSummary.tsx
@@ -203,8 +203,6 @@ export const DayDialogSummaryTabs: React.FC<{day: string}> = ({day}) => {
 
 function getHabitsAddedAtThisDay(habits: Habit[], day: string | Date): Habit[] {
 	return habits.filter(habit => {
-		// TODO: make {created,updated}_at required
-		if (!habit.created_at) return false;
 		const createdAtDate = new Date(habit.created_at);
 		const dayDate = new Date(day);
 
--- frontend/src/selectors/getHabitsAvailableAtDay.ts
+++ frontend/src/selectors/getHabitsAvailableAtDay.ts
@@ -4,9 +4,6 @@ import {Habit} from "../interfaces/index";
 
 export function getHabitsAvailableAtThisDay(habits: Habit[], day: string | Date): Habit[] {
 	return habits.filter(habit => {
-		// TODO: make {created,updated}_at required
-		if (!habit.created_at) return false;
-
 		const createdAtDate = new Date(habit.created_at);
 		const dayDate = new Date(day);
 
