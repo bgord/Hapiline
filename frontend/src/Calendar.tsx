18b313e2c6941321768bfe6a655a761b817a6d80
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-06-09 10:11:31 +0200

    Disable next/previous calendar buttons if needed

--- frontend/src/Calendar.tsx
+++ frontend/src/Calendar.tsx
@@ -1,5 +1,6 @@
 import {useQuery} from "react-query";
 import React from "react";
+import {isSameMonth, subMonths} from "date-fns";
 
 import * as UI from "./ui";
 import {CalendarIcon} from "./ui/icons/Calendar";
@@ -69,28 +70,60 @@ export const Calendar: React.FC = () => {
 		};
 	});
 
+	const isCurrentMonth = monthOffset === 0;
+
+	const [firstAddedHabit] = [...trackedHabits].sort((a, b) =>
+		a.created_at.toString().localeCompare(b.created_at.toString()),
+	);
+
+	// Get the date of month that's currently displayed,
+	// convert firstAddedHabit?.created_at to a format that date-fns understands,
+	// and check if they are the same month
+	const isTheMonthFirstHabbitWasAdded = isSameMonth(
+		subMonths(new Date(), monthOffset),
+		new Date(firstAddedHabit?.created_at),
+	);
+
+	const disabledPreviousButtonTitle =
+		getMonthRequestState.status === "loading"
+			? "Loading..."
+			: isTheMonthFirstHabbitWasAdded
+			? "There are no habits added in the previous month"
+			: undefined;
+
+	const disabledNextButtonTitle =
+		getMonthRequestState.status === "loading"
+			? "Loading..."
+			: isCurrentMonth
+			? "You cannot access the next month yet"
+			: undefined;
+
 	return (
 		<UI.Column mt="24" crossAxis="center">
 			<UI.Row mb="24" bg="gray-2" px="72" py="12" width="auto">
 				<UI.Button
 					variant="outlined"
 					onClick={widget.setPreviousMonth}
-					disabled={getMonthRequestState.status === "loading"}
+					disabled={getMonthRequestState.status === "loading" || isTheMonthFirstHabbitWasAdded}
 					style={{width: "100px"}}
+					title={disabledPreviousButtonTitle}
 					mr="24"
 				>
 					Previous
 				</UI.Button>
+
 				<CalendarIcon />
 				<UI.Text ml="6" variant="bold">
 					{date}
 				</UI.Text>
+
 				<UI.Button
 					ml="24"
 					variant="outlined"
 					onClick={widget.setNextMonth}
-					disabled={getMonthRequestState.status === "loading"}
+					disabled={getMonthRequestState.status === "loading" || isCurrentMonth}
 					style={{width: "100px"}}
+					title={disabledNextButtonTitle}
 				>
 					Next
 				</UI.Button>
