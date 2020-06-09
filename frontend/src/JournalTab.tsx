b2dfb303db787a4ebc4a6072575008642003acb5
bartlomiej <bartlomiej.zdybel@gmail.com>
2020-06-09 22:39:30 +0200

    Ustawienie wysokosci textarea

--- frontend/src/JournalTab.tsx
+++ frontend/src/JournalTab.tsx
@@ -5,7 +5,11 @@ export const JournalTab = () => (
 	<UI.Row p="24">
 		<UI.Field width="100%">
 			<UI.Label htmlFor="journal">Journal</UI.Label>
-			<UI.Textarea id="journal" value="Ok, so that's it? That's all you wanted to say?" />
+			<UI.Textarea
+				style={{minHeight: "400px"}} //TODO: Adjust to new solution
+				id="journal"
+				value="Ok, so that's it? That's all you wanted to say?"
+			/>
 		</UI.Field>
 	</UI.Row>
 );
