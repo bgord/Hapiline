2e649e38a47bade3268b1dde8f82375c0e522082
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-06-12 20:40:15 +0200

    Allow collapsing streak lists on dashboard

--- frontend/src/DashboardWindow.tsx
+++ frontend/src/DashboardWindow.tsx
@@ -2,6 +2,10 @@ import {Link} from "react-router-dom";
 import {useQuery} from "react-query";
 import React from "react";
 import deepEqual from "fast-deep-equal";
+import VisuallyHidden from "@reach/visually-hidden";
+
+import {ChevronUpIcon} from "./ui/icons/ChevronUp";
+import {ChevronDownIcon} from "./ui/icons/ChevronDown";
 
 import {DayDialog} from "./DayDialog";
 import {DaySummaryChart} from "./DayDialogSummary";
@@ -15,6 +19,7 @@ import {useDocumentTitle} from "./hooks/useDocumentTitle";
 import {useErrorToast} from "./contexts/toasts-context";
 import {DashboardStreakStats, DashboardHabitVoteStatsForDateRanges} from "./interfaces/index";
 import {UrlBuilder} from "./services/url-builder";
+import {useToggle} from "./hooks/useToggle";
 import * as UI from "./ui";
 
 export const DashboardWindow = () => {
@@ -181,6 +186,8 @@ export const DashboardWindow = () => {
 const ProgressStreakList: React.FC<{
 	progressStreakStats: DashboardStreakStats["progress_streaks"];
 }> = ({progressStreakStats}) => {
+	const {on: isProgressStreakListVisible, toggle: toggleProgressStreakList} = useToggle(true);
+
 	if (progressStreakStats.length === 0) return null;
 
 	return (
@@ -190,35 +197,61 @@ const ProgressStreakList: React.FC<{
 				<UI.Badge style={{padding: "0 3px"}} ml="6" variant="neutral">
 					{progressStreakStats.length}
 				</UI.Badge>
+
+				{isProgressStreakListVisible && (
+					<UI.Button
+						ml="auto"
+						variant="bare"
+						title="Hide progress streak list"
+						onClick={toggleProgressStreakList}
+					>
+						<VisuallyHidden>Hide progress streak list</VisuallyHidden>
+						<ChevronUpIcon />
+					</UI.Button>
+				)}
+
+				{!isProgressStreakListVisible && (
+					<UI.Button
+						ml="auto"
+						variant="bare"
+						title="Show progress streak list"
+						onClick={toggleProgressStreakList}
+					>
+						<VisuallyHidden>Show progress streak list</VisuallyHidden>
+						<ChevronDownIcon />
+					</UI.Button>
+				)}
 			</UI.Row>
 
-			<UI.Column bt="gray-1">
-				<ExpandContractList max={5}>
-					{progressStreakStats.map(habit => (
-						<UI.Row py="12" by="gray-1" key={habit.id} mainAxis="end">
-							<UI.Text mr="auto" as={Link} to={UrlBuilder.dashboard.habit.preview(habit.id)}>
-								{habit.name}
-							</UI.Text>
+			{isProgressStreakListVisible && (
+				<UI.Column bt="gray-1">
+					<ExpandContractList max={5}>
+						{progressStreakStats.map(habit => (
+							<UI.Row py="12" by="gray-1" key={habit.id} mainAxis="end">
+								<UI.Text mr="auto" as={Link} to={UrlBuilder.dashboard.habit.preview(habit.id)}>
+									{habit.name}
+								</UI.Text>
 
-							{!habit.has_vote_for_today && (
-								<UI.Badge
-									as={Link}
-									to={UrlBuilder.dashboard.calendar.habitToday(habit.id)}
-									variant="neutral"
-									mx="12"
-									title="Vote for this habit"
-								>
-									No vote yet
+								{!habit.has_vote_for_today && (
+									<UI.Badge
+										as={Link}
+										to={UrlBuilder.dashboard.calendar.habitToday(habit.id)}
+										variant="neutral"
+										mx="12"
+										title="Vote for this habit"
+									>
+										No vote yet
+									</UI.Badge>
+								)}
+
+								<UI.Badge variant="positive">
+									{habit.progress_streak} {pluralize("day", habit.progress_streak)} progress streak
 								</UI.Badge>
-							)}
-
-							<UI.Badge variant="positive">
-								{habit.progress_streak} {pluralize("day", habit.progress_streak)} progress streak
-							</UI.Badge>
-						</UI.Row>
-					))}
-				</ExpandContractList>
-			</UI.Column>
+							</UI.Row>
+						))}
+					</ExpandContractList>
+				</UI.Column>
+			)}
 		</>
 	);
 };
@@ -226,44 +259,72 @@ const ProgressStreakList: React.FC<{
 const RegressStreakList: React.FC<{
 	regressStreakStats: DashboardStreakStats["regress_streaks"];
 }> = ({regressStreakStats}) => {
+	const {on: isRegressStreakListVisible, toggle: toggleRegressStreakList} = useToggle(true);
+
 	if (regressStreakStats.length === 0) return null;
 
 	return (
 		<>
-			<UI.Row mt="24" mb="24" crossAxis="center">
+			<UI.Row mt="24" crossAxis="center">
 				<UI.Header variant="extra-small">Regress streaks</UI.Header>
 				<UI.Badge style={{padding: "0 3px"}} ml="6" variant="neutral">
 					{regressStreakStats.length}
 				</UI.Badge>
+
+				{isRegressStreakListVisible && (
+					<UI.Button
+						ml="auto"
+						variant="bare"
+						title="Hide regress streak list"
+						onClick={toggleRegressStreakList}
+					>
+						<VisuallyHidden>Hide regress streak list</VisuallyHidden>
+						<ChevronUpIcon />
+					</UI.Button>
+				)}
+
+				{!isRegressStreakListVisible && (
+					<UI.Button
+						ml="auto"
+						variant="bare"
+						title="Show regress streak list"
+						onClick={toggleRegressStreakList}
+					>
+						<VisuallyHidden>Show regress streak list</VisuallyHidden>
+						<ChevronDownIcon />
+					</UI.Button>
+				)}
 			</UI.Row>
 
-			<UI.Column by="gray-1">
-				<ExpandContractList max={5}>
-					{regressStreakStats.map(habit => (
-						<UI.Row py="12" by="gray-1" key={habit.id}>
-							<UI.Text mr="auto" as={Link} to={UrlBuilder.dashboard.habit.preview(habit.id)}>
-								{habit.name}
-							</UI.Text>
+			{isRegressStreakListVisible && (
+				<UI.Column by="gray-1" mt="24">
+					<ExpandContractList max={5}>
+						{regressStreakStats.map(habit => (
+							<UI.Row py="12" by="gray-1" key={habit.id}>
+								<UI.Text mr="auto" as={Link} to={UrlBuilder.dashboard.habit.preview(habit.id)}>
+									{habit.name}
+								</UI.Text>
 
-							{!habit.has_vote_for_today && (
-								<UI.Badge
-									as={Link}
-									to={UrlBuilder.dashboard.calendar.habitToday(habit.id)}
-									variant="neutral"
-									mx="12"
-									title="Vote for this habit"
-								>
-									No vote yet
+								{!habit.has_vote_for_today && (
+									<UI.Badge
+										as={Link}
+										to={UrlBuilder.dashboard.calendar.habitToday(habit.id)}
+										variant="neutral"
+										mx="12"
+										title="Vote for this habit"
+									>
+										No vote yet
+									</UI.Badge>
+								)}
+
+								<UI.Badge variant="negative">
+									{habit.regress_streak} {pluralize("day", habit.regress_streak)} regress streak
 								</UI.Badge>
-							)}
-
-							<UI.Badge variant="negative">
-								{habit.regress_streak} {pluralize("day", habit.regress_streak)} regress streak
-							</UI.Badge>
-						</UI.Row>
-					))}
-				</ExpandContractList>
-			</UI.Column>
+							</UI.Row>
+						))}
+					</ExpandContractList>
+				</UI.Column>
+			)}
 		</>
 	);
 };
