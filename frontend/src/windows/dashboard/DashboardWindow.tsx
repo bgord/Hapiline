79bec52223e082b2d857a37a187fc1f673192826
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-06-13 11:20:37 +0200

    Move dashboard regress streak list to a separate component

--- frontend/src/windows/dashboard/DashboardWindow.tsx
+++ frontend/src/windows/dashboard/DashboardWindow.tsx
@@ -23,6 +23,7 @@ import {useToggle} from "../../hooks/useToggle";
 import * as UI from "../../ui";
 
 import {MotivationalText} from "./DashboardMotivationalText";
+import {DashboardRegressStreakList} from "./DashboardRegressStreakList";
 
 export const DashboardWindow = () => {
 	useDocumentTitle("Hapiline - dashboard");
@@ -161,7 +162,7 @@ export const DashboardWindow = () => {
 				</UI.ShowIf>
 
 				<UI.ShowIf request={getDashboardStatsRequestState} is="success">
-					<RegressStreakList regressStreakStats={regressStreakStats} />
+					<DashboardRegressStreakList regressStreakStats={regressStreakStats} />
 					<ProgressStreakList progressStreakStats={progressStreakStats} />
 					<NoStreakList noStreakStats={noStreakStats} />
 				</UI.ShowIf>
@@ -251,80 +252,6 @@ const ProgressStreakList: React.FC<{
 								<UI.Badge variant="positive">
 									{habit.progress_streak}
 									{pluralize("day", habit.progress_streak)} progress streak
-								</UI.Badge>
-							</UI.Row>
-						))}
-					</ExpandContractList>
-				</UI.Column>
-			)}
-		</>
-	);
-};
-
-const RegressStreakList: React.FC<{
-	regressStreakStats: DashboardStreakStats["regress_streaks"];
-}> = ({regressStreakStats}) => {
-	const {on: isRegressStreakListVisible, toggle: toggleRegressStreakList} = useToggle(true);
-
-	if (regressStreakStats.length === 0) return null;
-
-	return (
-		<>
-			<UI.Row mt="24" crossAxis="center">
-				<UI.Header variant="extra-small">Regress streaks</UI.Header>
-				<UI.Badge style={{padding: "0 3px"}} ml="6" variant="neutral">
-					{regressStreakStats.length}
-				</UI.Badge>
-
-				{isRegressStreakListVisible && (
-					<UI.Button
-						ml="auto"
-						variant="bare"
-						title="Hide regress streak list"
-						onClick={toggleRegressStreakList}
-					>
-						<VisuallyHidden>Hide regress streak list</VisuallyHidden>
-						<ChevronUpIcon />
-					</UI.Button>
-				)}
-
-				{!isRegressStreakListVisible && (
-					<UI.Button
-						ml="auto"
-						variant="bare"
-						title="Show regress streak list"
-						onClick={toggleRegressStreakList}
-					>
-						<VisuallyHidden>Show regress streak list</VisuallyHidden>
-						<ChevronDownIcon />
-					</UI.Button>
-				)}
-			</UI.Row>
-
-			{isRegressStreakListVisible && (
-				<UI.Column by="gray-1" mt="24">
-					<ExpandContractList max={5}>
-						{regressStreakStats.map(habit => (
-							<UI.Row py="12" by="gray-1" key={habit.id}>
-								<UI.Text mr="auto" as={Link} to={UrlBuilder.dashboard.habit.preview(habit.id)}>
-									{habit.name}
-								</UI.Text>
-
-								{!habit.has_vote_for_today && (
-									<UI.Badge
-										as={Link}
-										to={UrlBuilder.dashboard.calendar.habitToday(habit.id)}
-										variant="neutral"
-										mx="12"
-										title="Vote for this habit"
-									>
-										No vote yet
-									</UI.Badge>
-								)}
-
-								<UI.Badge variant="negative">
-									{habit.regress_streak}
-									{pluralize("day", habit.regress_streak)} regress streak
 								</UI.Badge>
 							</UI.Row>
 						))}
