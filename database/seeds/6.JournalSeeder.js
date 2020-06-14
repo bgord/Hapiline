27e6996c20d109abb377718d14a89b3dc9b07e5a
bartlomiej <bartlomiej.zdybel@gmail.com>
2020-06-14 23:35:58 +0200

    Introduce changes after review

--- database/seeds/6.JournalSeeder.js
+++ database/seeds/6.JournalSeeder.js
@@ -1,24 +1,14 @@
-const datefns = require("date-fns");
 const Database = use("Database");
-const today = Date.now();
-
+const GEN_INT = 10;
 class JournalSeeder {
 	async run() {
 		const users = await Database.table("users");
 
 		for (let user of users) {
-			const ts = datefns
-				.subDays(today, Math.ceil(Math.random()) % 3)
-				.setHours(Math.ceil(Math.random()) & 3);
-			const date = new Date(ts).toISOString();
 			const payload = {
 				user_id: user.id,
-				content: `${Math.ceil(Math.random())} ${"lorem ipsum".repeat(
-					(Math.ceil(Math.random()) % 3) + 1,
-				)}`,
+				content: `${GEN_INT} ${"lorem ipsum".repeat((GEN_INT % 3) + 1)}`,
 				day: new Date().toISOString(),
-				created_at: date,
-				updated_at: date,
 			};
 			await Database.into("journals").insert(payload);
 		}
