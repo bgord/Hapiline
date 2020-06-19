8cbbfd7ba2e95b4052147df59bda65e3267a248a
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-06-19 16:46:51 +0200

    Make regress streak list responsive

--- frontend/src/windows/dashboard/DashboardRegressStreakList.tsx
+++ frontend/src/windows/dashboard/DashboardRegressStreakList.tsx
@@ -60,26 +60,40 @@ export const DashboardRegressStreakList: React.FC<{
 				<UI.Column by="gray-1" mt="24">
 					<ExpandContractList max={5}>
 						{regressStreakStats.map(habit => (
-							<UI.Row py="12" by="gray-1" key={habit.id}>
-								<UI.Text mr="auto" as={Link} to={UrlBuilder.dashboard.habit.preview(habit.id)}>
+							<UI.Row
+								mainAxis="between"
+								pb="12"
+								by="gray-1"
+								key={habit.id}
+								wrap={["nowrap", "wrap"]}
+								width="100%"
+							>
+								<UI.Text
+									mr="12"
+									mt="12"
+									as={Link}
+									to={UrlBuilder.dashboard.habit.preview(habit.id)}
+								>
 									{habit.name}
 								</UI.Text>
 
-								{!habit.has_vote_for_today && (
-									<UI.Badge
-										as={Link}
-										to={UrlBuilder.dashboard.calendar.habitToday(habit.id)}
-										variant="neutral"
-										mx="12"
-										title="Vote for this habit"
-									>
-										No vote yet
-									</UI.Badge>
-								)}
+								<UI.Row width="auto" mt="12">
+									{!habit.has_vote_for_today && (
+										<UI.Badge
+											as={Link}
+											to={UrlBuilder.dashboard.calendar.habitToday(habit.id)}
+											variant="neutral"
+											mr="12"
+											title="Vote for this habit"
+										>
+											No vote yet
+										</UI.Badge>
+									)}
 
-								<UI.Badge variant="negative">
-									{habit.regress_streak} {pluralize("day", habit.regress_streak)} regress streak
-								</UI.Badge>
+									<UI.Badge variant="negative">
+										{habit.regress_streak} {pluralize("day", habit.regress_streak)} regress streak
+									</UI.Badge>
+								</UI.Row>
 							</UI.Row>
 						))}
 					</ExpandContractList>
