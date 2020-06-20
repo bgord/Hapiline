0186fe2c552a013042527dd127aa1f81cfd3509b
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-06-20 17:58:22 +0200

    Add regenerate_manifest script

---
+++ scripts/regenerate_manifest.ts
@@ -0,0 +1,53 @@
+import fs from "fs";
+
+import manifest from "../public/manifest.json";
+
+async function main() {
+	const currentAppVersion = process.env.npm_package_version;
+
+	if (
+		!currentAppVersion ||
+		typeof currentAppVersion !== "string" ||
+		currentAppVersion.length === 0
+	) {
+		console.log("Cannot establish current app version, quitting...");
+		process.exit(1);
+	}
+
+	console.log(`App version: ${currentAppVersion}`);
+	console.log(`Manifest version: ${manifest.version}`);
+
+	if (currentAppVersion === manifest.version) {
+		console.log("\nManifest file is up to date, nothing to do.");
+		process.exit(5);
+	}
+
+	console.log("\nGenerating new manifest...");
+
+	const newManifest = constructManifest(currentAppVersion);
+	await fs.promises.writeFile("public/manifest.json", JSON.stringify(newManifest));
+
+	console.log("Manifest regenerated!");
+}
+
+function constructManifest(version: string) {
+	return {
+		manifest_version: 2,
+		name: "Hapiline",
+		short_name: "Hapiline - habit tracker",
+		start_url: "http://localhost:3333/",
+		display: "standalone",
+		theme_color: "#1654ff",
+		background_color: "#ecedf1",
+		orientation: "portrait-primary",
+		icons: [
+			{
+				src: "/app-icon.png",
+				sizes: "144x144",
+			},
+		],
+		version,
+	};
+}
+
+main().catch(console.log);
