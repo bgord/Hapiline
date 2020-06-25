f825371ce8e42228a82f4666a172623b1ab63172
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-06-25 20:09:00 +0200

    Make Header polymorphic

--- frontend/src/ui/header/Header.tsx
+++ frontend/src/ui/header/Header.tsx
@@ -1,6 +1,7 @@
 import "./header.css";
 
 import React from "react";
+import {Box, PolymorphicComponentProps} from "react-polymorphic-box";
 
 import {Margins, Paddings, getMarginTokens, getPaddingTokens} from "../design-system";
 
@@ -11,35 +12,34 @@ type HeaderVariants = {
 	variant?: ResponsiveHeaderVariant;
 };
 
-type HeaderProps = JSX.IntrinsicElements["header"] & HeaderVariants & Margins & Paddings;
+type HeaderOwnProps = JSX.IntrinsicElements["header"] & HeaderVariants & Margins & Paddings;
+
+export type HeaderProps<E extends React.ElementType> = PolymorphicComponentProps<E, HeaderOwnProps>;
+
+const defaultElement = "h1";
 
 // prettier-ignore
-export const Header: React.FC<HeaderProps> = ({
+export function Header<E extends React.ElementType = typeof defaultElement>({
 	variant = "medium",
 	m, mx, my, mt, mr, mb, ml,
-	p,
-	px,
-	py,
-	pt,
-	pr,
-	pb,
-	pl,
+	p, px, py, pt, pr, pb, pl,
 	...props
-}) => {
+}: HeaderProps<E>): JSX.Element {
 	const marginTokens = getMarginTokens({m, mx, my, mt, mr, mb, ml});
 	const paddingTokens = getPaddingTokens({p, px, py, pt, pr, pb, pl});
-  const variantTokens = getHeaderVariantTokens(variant)
+	const variantTokens = getHeaderVariantTokens(variant);
 
 	return (
-		<header
+		<Box
+			as={defaultElement}
 			className="c-header"
-      {...variantTokens}
+			{...variantTokens}
 			{...marginTokens}
 			{...paddingTokens}
 			{...props}
 		/>
 	);
-};
+}
 
 function getHeaderVariantTokens(variant: ResponsiveHeaderVariant) {
 	if (Array.isArray(variant)) {
