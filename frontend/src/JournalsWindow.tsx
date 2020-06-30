edb835790c6e03f243486e4b606bc3b925ec72c2
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-06-30 09:43:35 +0200

    Setup journals card

--- frontend/src/JournalsWindow.tsx
+++ frontend/src/JournalsWindow.tsx
@@ -1,5 +1,25 @@
 import React from "react";
 
+import * as UI from "./ui/";
+
 export function JournalsWindow() {
-	return <div>Journals window</div>;
+	return (
+		<UI.Card
+			as="main"
+			tabIndex={0}
+			pt="12"
+			mx={["auto", "6"]}
+			mt={["48", "12"]}
+			mb="24"
+			width={["view-l", "auto"]}
+		>
+			<UI.Row bg="gray-1" p={["24", "12"]}>
+				<UI.Header variant={["large", "small"]}>Journals</UI.Header>
+			</UI.Row>
+
+			<UI.Column p="24" px={["24", "6"]}>
+				journals
+			</UI.Column>
+		</UI.Card>
+	);
 }
