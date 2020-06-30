0fa8b2095aeda7f3946f4980f0cac2a10384c614
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-06-30 10:16:41 +0200

    Display basic non-empty journal row

--- frontend/src/JournalsWindow.tsx
+++ frontend/src/JournalsWindow.tsx
@@ -3,6 +3,8 @@ import {useQuery} from "react-query";
 
 import {Journal} from "./models";
 import {api} from "./services/api";
+import {formatDay, formatDayName} from "./services/date-formatter";
+import {pluralize} from "./services/pluralize";
 import {useErrorToast} from "./contexts/toasts-context";
 import * as UI from "./ui/";
 
@@ -17,6 +19,13 @@ export function JournalsWindow() {
 			retry: false,
 		},
 	});
+
+	const journals = getJournalsRequestState.data ?? [];
+
+	const journalsWithWordCount = journals.map(journal => ({
+		wordCount: journal.content ? journal.content.split(" ").length : 0,
+		...journal,
+	}));
 
 	return (
 		<UI.Card
@@ -39,6 +48,28 @@ export function JournalsWindow() {
 
 				<UI.ShowIf request={getJournalsRequestState} is="error">
 					<UI.ErrorBanner>Cannot fetch journals, please try again</UI.ErrorBanner>
+				</UI.ShowIf>
+
+				<UI.ShowIf request={getJournalsRequestState} is="success">
+					<UI.Text ml="auto" mb="24">
+						<UI.Text variant="bold">{journals.length}</UI.Text> results
+					</UI.Text>
+
+					<UI.ExpandContractList max={20}>
+						{journalsWithWordCount.map(journal => (
+							<UI.Row key={journal.id} crossAxis="baseline" by="gray-1" py="12">
+								<UI.Text variant="semi-bold" mr="12">
+									{formatDay(journal.day)}
+								</UI.Text>
+								<UI.Text variant="regular">{formatDayName(journal.day)}</UI.Text>
+
+								<UI.Text variant="bold" mr="24" ml="auto">
+									{journal.wordCount} {pluralize("word", journal.wordCount)}
+								</UI.Text>
+								<UI.Button variant="outlined">Show</UI.Button>
+							</UI.Row>
+						))}
+					</UI.ExpandContractList>
 				</UI.ShowIf>
 			</UI.Column>
 		</UI.Card>
--- frontend/src/services/pluralize.ts
+++ frontend/src/services/pluralize.ts
@@ -1,5 +1,10 @@
 export function pluralize(word: string, number: number, plural?: string) {
-	if (number === 0 || number === 1) return word;
+	if (number === 0) {
+		if (plural) return plural;
+		return `${word}s`;
+	}
+
+	if (number === 1) return word;
 
 	if (plural) return plural;
 
