a970d647169b6d1301801025327ae1e62e6cc0d6
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-07-10 07:31:45 +0200

    Add sort journal by option

--- frontend/src/services/journal.ts
+++ frontend/src/services/journal.ts
@@ -2,7 +2,7 @@ import {_internal_api} from "./api";
 import {constructUrl} from "../hooks/useQueryParam";
 import {formatDay} from "../services/date-formatter";
 
-import {Journal, DraftJournal} from "../models";
+import {Journal, DraftJournal, SortJournalByOption} from "../models";
 
 export const getJournalRequest = async (_key: "journal", day: Journal["day"]) =>
 	_internal_api
@@ -17,5 +17,5 @@ export const updateJournalRequest = (newJournalPayload: DraftJournal) =>
 		})
 		.then(response => response.data);
 
-export const getJournalsRequest = async (_key: "journals") =>
-	_internal_api.get<Journal[]>("/journals").then(response => response.data);
+export const getJournalsRequest = async (_key: "journals", sort: SortJournalByOption) =>
+	_internal_api.get<Journal[]>(constructUrl("/journals", {sort})).then(response => response.data);
