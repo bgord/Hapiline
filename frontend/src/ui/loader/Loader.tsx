8e59ca16f26401f0e31f583d38b47559994e2f53
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-07-28 19:05:53 +0200

    Create full-screen loader

--- frontend/src/ui/loader/Loader.tsx
+++ frontend/src/ui/loader/Loader.tsx
@@ -1,30 +1,14 @@
 import React from "react";
 
-export function Loader(props: React.SVGProps<SVGSVGElement>) {
+import * as UI from "../";
+
+export function Loader() {
 	return (
-		<svg
-			data-on-entry="rotate"
-			width="50"
-			height="50"
-			xmlns="http://www.w3.org/2000/svg"
-			{...props}
-		>
-			<defs>
-				<mask id="slice">
-					<rect width="100%" height="100%" fill="white" />
-					<rect height="50%" width="50%" fill="black" />
-				</mask>
-			</defs>
-			<circle
-				className="c-loader"
-				cx="25"
-				cy="25"
-				r="20"
-				stroke-width="4"
-				stroke="var(--brand-blue)"
-				fill="none"
-				mask="url(#slice)"
-			/>
-		</svg>
+		<UI.Row>
+			<UI.Column crossAxis="center" mx="auto" mt="72">
+				<UI.LoaderIcon data-mt="72" />
+				<UI.Text mt="6">Loading...</UI.Text>
+			</UI.Column>
+		</UI.Row>
 	);
 }
