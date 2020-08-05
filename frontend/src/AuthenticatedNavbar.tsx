9e6ccae3ba54fea3f0bb9508f195c93ad3be4045
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-08-05 19:33:56 +0200

    Add shortcuts for authenticated navbar items

--- frontend/src/AuthenticatedNavbar.tsx
+++ frontend/src/AuthenticatedNavbar.tsx
@@ -1,4 +1,4 @@
-import {NavLink} from "react-router-dom";
+import {NavLink, useHistory} from "react-router-dom";
 import * as React from "react";
 
 import * as UI from "./ui";
@@ -8,7 +8,20 @@ import {NotificationDropdown} from "./NotificationsDropdown";
 import {useWindowWidth} from "./ui/breakpoints";
 import {Menu, MenuList, MenuButton, MenuLink} from "@reach/menu-button";
 
+import {useKeyboardShortcurts} from "./hooks/useKeyboardShortcuts";
+
 export function AuthenticatedNavbar() {
+	const history = useHistory();
+
+	useKeyboardShortcurts({
+		"Shift+KeyD": () => history.push("/dashboard"),
+		"Shift+KeyH": () => history.push("/habits"),
+		"Shift+KeyC": () => history.push("/calendar"),
+		"Shift+KeyJ": () => history.push("/journals"),
+		"Shift+KeyM": () => history.push("/profile"),
+		"Shift+KeyL": () => history.push("/logout"),
+	});
+
 	const width = useWindowWidth();
 	const shouldDisplayInlineMenu = width > 1050;
 
@@ -22,7 +35,11 @@ export function AuthenticatedNavbar() {
 
 			<NotificationDropdown />
 
-			{shouldDisplayInlineMenu && <UI.NavItem to="/logout">Logout</UI.NavItem>}
+			{shouldDisplayInlineMenu && (
+				<UI.NavItem to="/logout" title={`Press "Shift + L"`}>
+					Logout
+				</UI.NavItem>
+			)}
 
 			{!shouldDisplayInlineMenu && <DropdownMenu />}
 		</UI.Row>
@@ -34,11 +51,19 @@ function InlineMenu() {
 
 	return (
 		<>
-			<UI.NavItem to="/dashboard">Dashboard</UI.NavItem>
-			<UI.NavItem to="/habits">Habits</UI.NavItem>
-			<UI.NavItem to="/calendar">Calendar</UI.NavItem>
-			<UI.NavItem to="/journals">Journals</UI.NavItem>
-			<UI.NavItem variant="bold" to="/profile">
+			<UI.NavItem to="/dashboard" title={`Press "Shift + D"`}>
+				Dashboard
+			</UI.NavItem>
+			<UI.NavItem to="/habits" title={`Press "Shift + H"`}>
+				Habits
+			</UI.NavItem>
+			<UI.NavItem to="/calendar" title={`Press "Shift + C"`}>
+				Calendar
+			</UI.NavItem>
+			<UI.NavItem to="/journals" title={`Press "Shift + J"`}>
+				Journals
+			</UI.NavItem>
+			<UI.NavItem variant="bold" to="/profile" title={`Press "Shift + M"`}>
 				{profile?.email}
 			</UI.NavItem>
 		</>
@@ -59,20 +84,20 @@ function DropdownMenu() {
 					<UI.Text variant="bold">{profile?.email}</UI.Text>
 				</MenuLink>
 
-				<MenuLink as={UI.NavItem} p="12" to="/dashboard">
+				<MenuLink as={UI.NavItem} p="12" to="/dashboard" title={`Press "Shift + D"`}>
 					Dashboard
 				</MenuLink>
-				<MenuLink as={UI.NavItem} p="12" to="/habits">
+				<MenuLink as={UI.NavItem} p="12" to="/habits" title={`Press "Shift + H"`}>
 					Habits
 				</MenuLink>
-				<MenuLink as={UI.NavItem} p="12" to="/calendar">
+				<MenuLink as={UI.NavItem} p="12" to="/calendar" title={`Press "Shift + C"`}>
 					Calendar
 				</MenuLink>
-				<MenuLink as={UI.NavItem} p="12" to="/journals">
+				<MenuLink as={UI.NavItem} p="12" to="/journals" title={`Press "Shift + J"`}>
 					Journals
 				</MenuLink>
 
-				<MenuLink as={UI.NavItem} p="12" to="/logout">
+				<MenuLink as={UI.NavItem} p="12" to="/logout" title={`Press "Shift + L"`}>
 					Logout
 				</MenuLink>
 			</UI.Card>
