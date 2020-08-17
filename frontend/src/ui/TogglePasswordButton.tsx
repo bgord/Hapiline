49fba60af7d48fdcad073f1fa610721e7c94b53f
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-08-17 08:21:17 +0200

    Add TogglePasswordButton component

---
+++ frontend/src/ui/TogglePasswordButton.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {useToggle} from "../hooks/useToggle";
+import * as UI from "./";
+import {User} from "../models";
+
+export function useTogglePassword(
+	password: User["password"],
+): [
+	{
+		isPasswordVisible: ReturnType<typeof useToggle>["on"];
+		onClick: JSX.IntrinsicElements["button"]["onClick"];
+		disabled: JSX.IntrinsicElements["button"]["disabled"];
+	},
+	{type: TogglePasswordInputType},
+] {
+	const {on: isPasswordVisible, toggle: togglePasswordVisible} = useToggle();
+
+	const togglePasswordButtonProps = {
+		isPasswordVisible,
+		onClick: togglePasswordVisible,
+		disabled: password.length === 0,
+	};
+
+	const togglePasswordInputProps: {type: TogglePasswordInputType} = {
+		type: isPasswordVisible ? "text" : "password",
+	};
+
+	return [togglePasswordButtonProps, togglePasswordInputProps];
+}
+
+export function TogglePasswordButton(
+	props: {isPasswordVisible: ReturnType<typeof useToggle>["on"]} & JSX.IntrinsicElements["button"],
+) {
+	const {isPasswordVisible, ref, ...rest} = props;
+
+	return (
+		<UI.Button ml="3" variant="bare" data-bg="gray-1" style={{width: "70px"}} {...rest}>
+			{isPasswordVisible ? "Hide" : "Show"}
+		</UI.Button>
+	);
+}
+
+type TogglePasswordInputType = "text" | "password";
