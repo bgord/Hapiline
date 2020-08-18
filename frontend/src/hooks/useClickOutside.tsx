b6823e9a0a494e1e659754ab2c849cdf0fb3ab82
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-08-18 14:04:36 +0200

    Comment useClickOutside hook

--- frontend/src/hooks/useClickOutside.tsx
+++ frontend/src/hooks/useClickOutside.tsx
@@ -9,11 +9,17 @@ export function useClickOutside(
 		if (!ref.current) return;
 
 		function handleClickOutside(event: MouseEvent) {
+			// Check if click event happened outside the `ref`
+			// so the `onClickOutside` callback `may` be fired.
 			if (!ref.current?.contains(event.target as Node)) {
+				// The second check is to check if some `exclude`d node
+				// outside the `ref` node has been clicked.
 				const isExcludedNodeClicked = exclude?.some(node =>
 					node.current?.contains(event.target as Node),
 				);
 
+				// `onClickOutside` callback is fired if the click event
+				// happened outside both `ref` node and `exclude`d nodes.
 				if (!isExcludedNodeClicked) {
 					onClickOutside();
 				}
