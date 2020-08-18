f0a461ee660bd52aa6161792a5f1faebdb9ab19f
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-08-18 12:45:18 +0200

    Make UI.Card accept ref

--- frontend/src/ui/card/Card.tsx
+++ frontend/src/ui/card/Card.tsx
@@ -22,8 +22,7 @@ import {
 	getOnEntryAnimationToken,
 } from "../design-system";
 
-type CardOwnProps = JSX.IntrinsicElements["div"] &
-	Paddings &
+type CardOwnProps = Paddings &
 	Margins &
 	Positions &
 	Backgrounds &
@@ -37,39 +36,42 @@ export type CardProps<E extends React.ElementType> = PolymorphicComponentProps<E
 const defaultElement = "div";
 
 // prettier-ignore
-export function Card<E extends React.ElementType = typeof defaultElement>({
-	m, mx, my, mt, mr, mb, ml,
-	p, px, py, pt, pr, pb, pl,
-	position = "static",
-	bg,
-	width,
-	z,
-	overflow,
-	onEntry,
-	...props
-}: CardProps<E>): JSX.Element {
-	const marginTokens = getMarginTokens({m, mx, my, mt, mr, mb, ml});
-	const paddingTokens = getPaddingTokens({p, px, py, pt, pr, pb, pl});
-	const positionToken = getPositionToken(position);
-	const backgroundToken = getBackgroundToken(bg);
-	const widthToken = getWidthToken(width);
-	const zIndexToken = getZIndexToken(z);
-	const overflowToken = getOverflowToken(overflow);
-	const onEntryAnimationToken = getOnEntryAnimationToken(onEntry);
+export const Card = React.forwardRef(
+	<E extends React.ElementType = typeof defaultElement>(
+		{
+			ref,
+			m, mx, my, mt, mr, mb, ml,
+			p, px, py, pt, pr, pb, pl,
+			position = "static",
+			bg, width, z, overflow, onEntry,
+			...props
+		}: CardProps<E>,
+		innerRef: typeof ref,
+	) => {
+		const marginTokens = getMarginTokens({m, mx, my, mt, mr, mb, ml});
+		const paddingTokens = getPaddingTokens({p, px, py, pt, pr, pb, pl});
+		const positionToken = getPositionToken(position);
+		const backgroundToken = getBackgroundToken(bg);
+		const widthToken = getWidthToken(width);
+		const zIndexToken = getZIndexToken(z);
+		const overflowToken = getOverflowToken(overflow);
+		const onEntryAnimationToken = getOnEntryAnimationToken(onEntry);
 
-	return (
-		<Box
-			as={defaultElement}
-			className="c-card"
-			{...marginTokens}
-			{...paddingTokens}
-			{...positionToken}
-			{...backgroundToken}
-			{...widthToken}
-			{...zIndexToken}
-			{...overflowToken}
-			{...onEntryAnimationToken}
-			{...props}
-		/>
-	);
-}
+		return (
+			<Box
+				as={defaultElement}
+				className="c-card"
+				ref={innerRef}
+				{...marginTokens}
+				{...paddingTokens}
+				{...positionToken}
+				{...backgroundToken}
+				{...widthToken}
+				{...zIndexToken}
+				{...overflowToken}
+				{...onEntryAnimationToken}
+				{...props}
+			/>
+		);
+	},
+) as <E extends React.ElementType = typeof defaultElement>(props: CardProps<E>) => JSX.Element;
