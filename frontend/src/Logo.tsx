1a1c6ce837ee4fb3f7fb752468558593c292e21e
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-08-29 13:42:14 +0200

    Fix incorrect logo on mobile

--- frontend/src/Logo.tsx
+++ frontend/src/Logo.tsx
@@ -2,17 +2,14 @@ import React from "react";
 
 import {BREAKPOINTS} from "./ui/breakpoints";
 
-export function Logo(props: JSX.IntrinsicElements["img"]) {
+export function Logo() {
 	const fullLogoFilename = "logo.png";
 	const iconLogoFilename = "logo-icon.png";
 
 	return (
-		<img
-			alt="Hapiline brand"
-			src={fullLogoFilename}
-			srcSet={`${fullLogoFilename} 181w, ${iconLogoFilename} 50w`}
-			sizes={`(max-width: ${BREAKPOINTS.lg}px) 50px, 181px`}
-			{...props}
-		/>
+		<picture>
+			<source srcSet={fullLogoFilename} media={`(min-width: ${BREAKPOINTS.lg}px)`} />
+			<img src={iconLogoFilename} alt="Hapiline brand" />
+		</picture>
 	);
 }
