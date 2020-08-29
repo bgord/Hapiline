8ff838c8536d78b6c83b87aa1e3835837bfd5462
Bartosz Gordon <bartosz.gordon@sealcode.org>
2020-08-29 13:19:38 +0200

    Improve variable names

--- frontend/src/Logo.tsx
+++ frontend/src/Logo.tsx
@@ -3,14 +3,14 @@ import React from "react";
 import {BREAKPOINTS} from "./ui/breakpoints";
 
 export function Logo(props: JSX.IntrinsicElements["img"]) {
-	const fullLogo = "logo.png";
-	const iconLogo = "logo-icon.png";
+	const fullLogoFilename = "logo.png";
+	const iconLogoFilename = "logo-icon.png";
 
 	return (
 		<img
 			alt="Hapiline brand"
-			src={fullLogo}
-			srcSet={`${fullLogo} 181w, ${iconLogo} 50w`}
+			src={fullLogoFilename}
+			srcSet={`${fullLogoFilename} 181w, ${iconLogoFilename} 50w`}
 			sizes={`(max-width: ${BREAKPOINTS.lg}px) 50px, 181px`}
 			{...props}
 		/>
